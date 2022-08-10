a6b83c5a22255c82cc3a1134596619580dd3fafe
w4yZ <68845415+webwebweb5@users.noreply.github.com>
2022-08-10 22:18:41 +0700

    Add Exercise 1

--- SE233/Chapter 3/main/java/se233/chapter3reverseindexcreation/model/FileFreq.java
+++ SE233/Chapter 3/main/java/se233/chapter3reverseindexcreation/model/FileFreq.java
@@ -1,5 +1,8 @@
 package se233.chapter3reverseindexcreation.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class FileFreq {
     private final String name;
     private final String path;
@@ -18,5 +21,21 @@ public class FileFreq {
 
     public String getPath() {
         return path;
+    }
+
+    public Integer getFreq() {
+        return freq;
+    }
+
+    // Exercise 1
+    // The result entries shown in the ListView on the right-hand side of the application window
+    // are sorted alphabetically. Your task is to order them in terms of their total frequency, where
+    // the word with the highest frequency count should come first. For the words appearing in
+    // many documents, use the total sum of frequencies.
+    public static class SortByFreq implements Comparator<ArrayList<FileFreq>> {
+        @Override
+        public int compare(ArrayList<FileFreq> o1,ArrayList<FileFreq> o2){
+            return o1.get(0).getFreq().compareTo(o2.get(0).getFreq());
+        }
     }
 }
